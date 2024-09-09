22759efe73e193f1a0a57011bf6b219b92a831a1
Revii28 <revirifaldi123@gmail.com>
2024-09-09 08:59:45 +0700

    feat: Add product

---
+++ data/dataProducts.js
@@ -0,0 +1,159 @@
+const products = [
+  { 
+      id: 1,
+      name: 'Kabel Data Charger Fast Charging 3 in 1 Kabel Lightning Micro USB Type C Black for Android IOS Type-c', 
+      price: 'Rp125.000', 
+      imgSrc: ['/asset/products/Kabel data 3 in 1/1.png', '/asset/products/Kabel data 3 in 1/2.png', '/asset/products/Kabel data 3 in 1/3.png'], 
+      description: `Fitur:（Dapat digunakan untuk mengisi daya laptop）
+
+      ①Output daya penuh 100W, luar biasa cepat. Dapat mengisi daya 90% dalam 30 menit. Kabel data dapat memenuhi semua tingkat daya pengisian cepat yang ada di pasaran dan kompatibel dengan 88w, 66w, 40w, dan 22,5w.
+      
+      ②Antarmuka USB 3.0, standar, kecepatan transfer file hingga 480MB/S
+      
+      ③5 baterai super-i tebal, tidak panas, pengisian lebih stabil dan lebih aman.
+      
+      ④Kualitasnya tahan uji, bantalan uji ayun 40KG 20000 kali
+      
+      ⑤Bahan silikon asli, lembut dan elastis, tepi dilapisi listrik, tidak mudah kusut dan bebas debu
+      
+      ⑥Port bundar logam satu bagian kuat dan tahan aus, dan konektor paduan seng seluruh logam dapat dipasang dan dicabut 1.000 kali sesuka hati.Ini akan terlihat seperti baru setelah penggunaan jangka panjang dan tidak akan mudah rusak.
+      
+      ⑦Kompatibel dengan IOS dan Android, transfer file hingga 480MB/s`,
+      rating: '4.8 (267 Penilaian)',
+      protection: 'Melindungi barang belanjaan dari kerusakan hingga 6 bulan',
+      // shipping: 'Gratis Ongkir',
+      colors: ['3in1 Hitam'],
+      stock: 199,
+      linkPayment: 'https://rheastore.orderonline.id/kabel-data-charger-fast-charging-3-in-1'
+  },
+  { 
+      id: 2,
+      name: 'kabel data 4in1 fast charging kabel cas 4in1 type c usb lightning tipe c', 
+      price: 'Rp66.000', 
+      imgSrc: ['/asset/products/Kabel data 4 in 1/gambar1.jpg', '/asset/products/Kabel data 4 in 1/gambar2.jpg', '/asset/products/Kabel data 4 in 1/gambar3.jpg'], 
+      description: `Kabel Super Fast Charge PD65W | Kabel 4 in 1 Fast Charge USB C USB A Multiple Charging | Charge Iphone dan Android
+
+      Kabel Data portable multifungsi dengan pengisian data super cepat
+      
+      Detail produk :
+      
+      - Cocok untuk ponsel, tablet, komputer, HP
+      
+      - Type Kabel : 4 in 1
+      
+      - USB to Lightning
+      
+      - USB to type C
+      
+      - Type C to Type C
+      
+      - Type C to Lightning
+      
+      - Panjang : 1 m
+      
+      - Daya : 65W fast charge
+      
+      - Lighting 27W + Android 65W anti kusut
+      
+      - Data transfer speed : <480mbps
+      
+      Produk sebelum dikirim sudah ditest dengan kondisi yang dipastikan aman, jika ada kerusakan kemungkinan dari pengiriman, karena paket yang terbanting.`,
+      rating: '4.7 (586 Penilaian)',
+      protection: 'Melindungi barang belanjaan dari kerusakan hingga 6 bulan',
+      // shipping: 'Gratis Ongkir',
+      colors: ['4in1 Gold-Hitam'],
+      stock: 199,
+      linkPayment: 'https://rheastore.orderonline.id/kabel-data-4in1-fast-charging-kabel-cas-4in1-type-c-usb-lightning-tipe-c'
+  },
+  { 
+      id: 3,
+      name: 'Sepatu Sneakers Keren PRIA/WANITA', 
+      price: 'Rp93.500', 
+      imgSrc: ['/asset/products/Sepatu Jepang/gambar1.jpg', '/asset/products/Sepatu Jepang/gambar2.jpg', '/asset/products/Sepatu Jepang/gambar3.jpg'], 
+      description: `DESKRIPSI PRODUK
+
+      Sepatu Sneakers Kualitas Bagus Harga Terjangkau
+      
+      Sepatu Jogger Musim Panas
+      
+      Sepatu Casual Kaki Bisa Bergerak Dengan Santai
+      
+      Sepatu Karet Lembut Didalam Nyaman Saat Dipakai
+      
+      Sepatu Sport Model Simple Elegan Trend Fashion
+      
+      Rincian Ukuran :
+      
+      39 = 24.5 cm
+      
+      40 = 25 cm
+      
+      41 = 25.5 cm
+      
+      42 = 26 cm
+      
+      43 = 26.5 cm
+      
+      warna:
+      
+      -kuning
+      
+      -putih
+      
+      -hitam
+      
+      Mohon Untuk Diperhatikan :
+      
+      - Warna Sesuai Dengan Gambar
+      
+      - Ukuran Sesuai Dengan Kaki
+      
+      - Gambar Sesuai Asli Barang Yang Difotokan
+      
+      Catatan : Kami haya mengirim barang sesuai dengan yang dimasukan ke keranjang belanja. Jika ingin complain barang WAJIB ada video Unboxing. Membeli berarti menyetujui , Terima Kasih.
+      
+      Happy shopping..`,
+      rating: '4.9 (846 Penilaian)',
+      protection: 'Melindungi barang belanjaan dari kerusakan hingga 6 bulan',
+      // shipping: 'Gratis Ongkir',
+      colors: [
+      'kuning - 39', 'kuning - 40', 'kuning - 41', 'kuning - 42', 'kuning - 43',
+      'putih - 39', 'putih - 40', 'putih - 41', 'putih - 42', 'putih - 43',
+      'hitam - 39', 'hitam - 40', 'hitam - 41', 'hitam - 42', 'hitam - 43',
+  ],
+      stock: 199,
+      linkPayment: 'https://rheastore.orderonline.id/sepatu-sneakers-jintu-pria-bg-115-1'
+  },
+  { 
+    id: 4,
+    name: 'Jeans Sobek Destroy Biowash Ukuran L XL', 
+    price: 'Rp148.750', 
+    imgSrc: [
+    '/asset/products/Jeans Sobek Destroy/gambar1.jpg',
+    '/asset/products/Jeans Sobek Destroy/gambar2.jpg',
+    '/asset/products/Jeans Sobek Destroy/gambar3.jpg',
+    '/asset/products/Jeans Sobek Destroy/gambar4.jpg',
+    '/asset/products/Jeans Sobek Destroy/gambar5.jpg',
+    '/asset/products/Jeans Sobek Destroy/gambar6.jpg',
+  ], 
+    description: `
+    Bahan : Jeans
+
+    L = Lebar 48cm X Panjang 62cm X Panjang Tangan 58cm
+    
+    XL = Lebar 53cm X Panjang 66cm X Panjang Tangan 61cm
+    
+    Berat Produk : 900gr
+    
+    SETIAP MINGGU TIDAK ADA PENGIRIMAN (LIBUR)
+    
+    (Barang Tidak Sesuai Pict / Gambar Boleh Tuker Kembali`,
+    rating: '4.9 (854 Penilaian)',
+    protection: 'Melindungi barang belanjaan dari kerusakan hingga 6 bulan',
+    // shipping: 'Gratis Ongkir',
+    colors: ['L', 'XL'],
+    stock: 199,
+    linkPayment: 'https://rheastore.orderonline.id/jeans-sobek-destroy-biowash-ukuran-l-xl-1'
+},
+  
+];
